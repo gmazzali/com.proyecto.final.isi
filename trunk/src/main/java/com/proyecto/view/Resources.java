cc2e90c0423a8117a1b39b08fb9ae12e2acac7e3
guillermomazzali@gmail.com <guillermomazzali@gmail.com@4db54f59-3118-ff34-ce64-8256e353c912>
2013-08-06 18:06:12 +0000

    Cambios en la ventana de listado de instrumentos y en la de edición de reactivos. Cambios en los recursos de los iconos.

--- trunk/src/main/java/com/proyecto/view/Resources.java
+++ trunk/src/main/java/com/proyecto/view/Resources.java
@@ -4,55 +4,67 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 
+/**
+ * La clase que contiene los iconos que vamos a utilizar dentro de la aplicación.
+ * 
+ * @author Guillermo Mazzali
+ * @version 1.0
+ */
 public class Resources {
 
 	/**
 	 * Los anchos y alto de las imágenes.
 	 */
-	private static Integer height = 15;
-	private static Integer width = 15;
+	private static Integer height = 30;
+	private static Integer width = 30;
 
 	/**
-	 * El icono para "nueva entidad".
+	 * El icono para agregar un nuevo elemento.
 	 */
-	public static ImageIcon ADD_ICON;
+	public static final ImageIcon ADD_ELEMENT_ICON = Resources.createImage("com/proyecto/image/add_32.png", Resources.width, Resources.height);
 	/**
-	 * El icono para "modificar entidad".
+	 * El icono para modificar un elemento.
 	 */
-	public static ImageIcon MODIFY_ICON;
+	public static final ImageIcon MODIFY_ELEMENT_ICON = Resources.createImage("com/proyecto/image/modify_32.png", Resources.width, Resources.height);
 	/**
-	 * El icono para "eliminar entidad".
+	 * El icono para eliminar un elemento.
 	 */
-	public static ImageIcon DELETE_ICON;
+	public static final ImageIcon DELETE_ELEMENT_ICON = Resources.createImage("com/proyecto/image/delete_32.png", Resources.width, Resources.height);
+
+	/**
+	 * El icono para refrescar los elementos.
+	 */
+	public static final ImageIcon REFRESH_ELEMENT_ICON = Resources.createImage("com/proyecto/image/refresh_32.png", Resources.width, Resources.height);
+	/**
+	 * El icono para seleccionar los elementos.
+	 */
+	public static final ImageIcon SELECT_ELEMENT_ICON = Resources.createImage("com/proyecto/image/select_32.png", Resources.width, Resources.height);
+	
 	/**
 	 * El icono para retornar.
 	 */
-	public static ImageIcon RETURN_ICON;
+	public static final ImageIcon CLOSE_ICON = Resources.createImage("com/proyecto/image/close_32.png", Resources.width, Resources.height);
 
 	/**
 	 * Los archivos de los gif de progresos.
 	 */
-	public static ImageIcon PROGRESS_LIST_ICON;
-	public static ImageIcon PROGRESS_SAVE_ICON;
-	public static ImageIcon PROGRESS_BAR_ICON;
+	public static final ImageIcon PROGRESS_LIST_ICON = Resources.createImage("com/proyecto/image/progress_list.gif", Resources.width, Resources.height);
+	public static final ImageIcon PROGRESS_SAVE_ICON = Resources.createImage("com/proyecto/image/progress_save.gif", Resources.width, Resources.height);
+	public static final ImageIcon PROGRESS_BAR_ICON = Resources.createImage("com/proyecto/image/progress_bar.gif", Resources.width, Resources.height);
 
-	static {
-		Resources.ADD_ICON = new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/add.png")).getImage()
-				.getScaledInstance(Resources.width, Resources.height, Image.SCALE_SMOOTH));
-
-		Resources.MODIFY_ICON = new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/modify.png")).getImage()
-				.getScaledInstance(Resources.width, Resources.height, Image.SCALE_SMOOTH));
-
-		Resources.DELETE_ICON = new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/delete.png")).getImage()
-				.getScaledInstance(Resources.width, Resources.height, Image.SCALE_SMOOTH));
-
-		Resources.RETURN_ICON = new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/return.png")).getImage()
-				.getScaledInstance(Resources.width, Resources.height, Image.SCALE_SMOOTH));
-
-		Resources.PROGRESS_LIST_ICON = new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/progress_list.gif"));
-
-		Resources.PROGRESS_SAVE_ICON = new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/progress_save.gif"));
-
-		Resources.PROGRESS_BAR_ICON = new ImageIcon(Resources.class.getClassLoader().getResource("com/proyecto/image/progress_bar.gif"));
+	/**
+	 * La función encargada de crear los iconos de acuerdo al path recibidos y al tamaño requerido de redimensión.
+	 * 
+	 * @param path
+	 *            El path donde se encuentra la imagen que vamos a utilizar para crear el icono.
+	 * @param width
+	 *            El ancho con el que vamos a redimensionar el icono a crear.
+	 * @param height
+	 *            El alto con el que vamos a redimensionar el icono a crear.
+	 * @return El icono que creamos de acuerdo a los parametros recibidos.
+	 */
+	public static ImageIcon createImage(String path, Integer width, Integer height) {
+		return new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource(path)).getImage().getScaledInstance(width, height,
+				Image.SCALE_SMOOTH));
 	}
 }
