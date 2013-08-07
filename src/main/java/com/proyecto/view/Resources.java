3374f34a19b1283590bb8373e7144acc8a2fbf85
guillermomazzali <guillermomazzali@gmail.com>
2013-08-07 04:12:17 +0000

    Cambios en los recursos de los iconos. Cambios en la ventana de reactivos y de listado de instrumentos.

--- src/main/java/com/proyecto/view/Resources.java
+++ src/main/java/com/proyecto/view/Resources.java
@@ -15,42 +15,44 @@ public class Resources {
 	/**
 	 * Los anchos y alto de las imágenes.
 	 */
-	private static Integer height = 30;
-	private static Integer width = 30;
+	private static Integer height = 20;
+	private static Integer width = 20;
 
 	/**
 	 * El icono para agregar un nuevo elemento.
 	 */
-	public static final ImageIcon ADD_ELEMENT_ICON = Resources.createImage("com/proyecto/image/add_32.png", Resources.width, Resources.height);
+	public static final ImageIcon ADD_ELEMENT_ICON = Resources.createStaticImage("com/proyecto/image/add_32.png", Resources.width, Resources.height);
 	/**
 	 * El icono para modificar un elemento.
 	 */
-	public static final ImageIcon MODIFY_ELEMENT_ICON = Resources.createImage("com/proyecto/image/modify_32.png", Resources.width, Resources.height);
+	public static final ImageIcon MODIFY_ELEMENT_ICON = Resources.createStaticImage("com/proyecto/image/modify_32.png", Resources.width,
+			Resources.height);
 	/**
 	 * El icono para eliminar un elemento.
 	 */
-	public static final ImageIcon DELETE_ELEMENT_ICON = Resources.createImage("com/proyecto/image/delete_32.png", Resources.width, Resources.height);
+	public static final ImageIcon DELETE_ELEMENT_ICON = Resources.createStaticImage("com/proyecto/image/delete_32.png", Resources.width,
+			Resources.height);
 
 	/**
 	 * El icono para refrescar los elementos.
 	 */
-	public static final ImageIcon REFRESH_ELEMENT_ICON = Resources.createImage("com/proyecto/image/refresh_32.png", Resources.width, Resources.height);
+	public static final ImageIcon REFRESH_ELEMENT_ICON = Resources.createStaticImage("com/proyecto/image/refresh_32.png", Resources.width,
+			Resources.height);
 	/**
 	 * El icono para seleccionar los elementos.
 	 */
-	public static final ImageIcon SELECT_ELEMENT_ICON = Resources.createImage("com/proyecto/image/select_32.png", Resources.width, Resources.height);
-	
-	/**
-	 * El icono para retornar.
-	 */
-	public static final ImageIcon CLOSE_ICON = Resources.createImage("com/proyecto/image/close_32.png", Resources.width, Resources.height);
+	public static final ImageIcon SELECT_ELEMENT_ICON = Resources.createStaticImage("com/proyecto/image/select_32.png", Resources.width,
+			Resources.height);
 
 	/**
-	 * Los archivos de los gif de progresos.
+	 * El icono para cerrar una ventana.
 	 */
-	public static final ImageIcon PROGRESS_LIST_ICON = Resources.createImage("com/proyecto/image/progress_list.gif", Resources.width, Resources.height);
-	public static final ImageIcon PROGRESS_SAVE_ICON = Resources.createImage("com/proyecto/image/progress_save.gif", Resources.width, Resources.height);
-	public static final ImageIcon PROGRESS_BAR_ICON = Resources.createImage("com/proyecto/image/progress_bar.gif", Resources.width, Resources.height);
+	public static final ImageIcon CLOSE_ICON = Resources.createStaticImage("com/proyecto/image/close_32.png", Resources.width, Resources.height);
+
+	/**
+	 * Los archivos de los GIF de progresos.
+	 */
+	public static final ImageIcon PROGRESS_LIST_ICON = Resources.createGifImage("com/proyecto/image/progress_list.gif");
 
 	/**
 	 * La función encargada de crear los iconos de acuerdo al path recibidos y al tamaño requerido de redimensión.
@@ -61,10 +63,21 @@ public class Resources {
 	 *            El ancho con el que vamos a redimensionar el icono a crear.
 	 * @param height
 	 *            El alto con el que vamos a redimensionar el icono a crear.
-	 * @return El icono que creamos de acuerdo a los parametros recibidos.
+	 * @return El icono que creamos de acuerdo a los parámetros recibidos.
 	 */
-	public static ImageIcon createImage(String path, Integer width, Integer height) {
+	public static ImageIcon createStaticImage(String path, Integer width, Integer height) {
 		return new ImageIcon(new ImageIcon(Resources.class.getClassLoader().getResource(path)).getImage().getScaledInstance(width, height,
 				Image.SCALE_SMOOTH));
+	}
+
+	/**
+	 * La función encargada de crear los iconos animado de acuerdo al path recibidos.
+	 * 
+	 * @param path
+	 *            El path donde se encuentra la imagen en formato GIF que vamos a utilizar para crear el icono.
+	 * @return El icono animado que creamos de acuerdo a los parámetros recibidos.
+	 */
+	public static ImageIcon createGifImage(String path) {
+		return new ImageIcon(Resources.class.getClassLoader().getResource(path));
 	}
 }
--- src/main/java/com/proyecto/view/reactive/ReactiveFormDialog.java
+++ src/main/java/com/proyecto/view/reactive/ReactiveFormDialog.java
@@ -3,19 +3,36 @@ package com.proyecto.view.reactive;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
-import com.proyecto.model.agent.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.common.util.annotations.View;
+import com.common.util.exception.CheckedException;
+import com.common.util.holder.HolderApplicationContext;
+import com.common.util.holder.HolderMessage;
+import com.proyecto.model.instrument.ChoiceInstrument;
+import com.proyecto.model.instrument.Instrument;
+import com.proyecto.model.instrument.type.InstrumentTypeInterface;
+import com.proyecto.model.instrument.type.impl.InstrumentType;
 import com.proyecto.model.reactive.Reactive;
 import com.proyecto.service.reactive.ReactiveService;
+import com.proyecto.view.Resources;
 import com.proyecto.view.instrument.InstrumentListDialog;
 
 /**
@@ -27,7 +44,7 @@ import com.proyecto.view.instrument.InstrumentListDialog;
 @View
 public class ReactiveFormDialog extends JDialog {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -3007425576330803557L;
 
 	/**
 	 * La ventana de selección de reactivo.
@@ -40,11 +57,27 @@ public class ReactiveFormDialog extends JDialog {
 	 */
 	@Autowired
 	private ReactiveService reactiveService;
-	
+
 	/**
 	 * El reactivo que estamos editando.
 	 */
 	private Reactive reactive;
+
+	/**
+	 * El combo con los tipos de instrumentos que vamos a usar dentro de las evaluaciones.
+	 */
+	private JComboBox<InstrumentTypeInterface> assessementTypeComboBox;
+	/**
+	 * Las areas donde vamos a desplegar las descripciones.
+	 */
+	private JTextArea descriptionTextArea;
+	private JTextArea instrumentTextArea;
+	/**
+	 * Los botones de acción.
+	 */
+	private JButton commitButton;
+	private JButton rejectButton;
+	private JButton addInstrumentbutton;
 
 	/**
 	 * Constructor de una ventana de edición de reactivos.
@@ -58,49 +91,127 @@ public class ReactiveFormDialog extends JDialog {
 	 * La función encargada de inicializar el contenido de la ventana.
 	 */
 	private void init() {
+		this.setModal(true);
+		this.setResizable(false);
 		JPanel contentPanel = new JPanel();
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPanel.setLayout(null);
 
-		setBounds(100, 100, 727, 396);
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		this.setBounds(100, 100, 727, 387);
+		this.getContentPane().setLayout(new BorderLayout());
+		this.getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+		JLabel selectionTypeEvaluationLabel = new JLabel("Tipo de Evaluaci\u00F3n");
+		selectionTypeEvaluationLabel.setFont(new Font("Arial", Font.BOLD, 11));
+		selectionTypeEvaluationLabel.setBounds(10, 11, 104, 14);
+		contentPanel.add(selectionTypeEvaluationLabel);
+
+		this.assessementTypeComboBox = new JComboBox<InstrumentTypeInterface>();
+		this.assessementTypeComboBox.setBounds(10, 27, 222, 30);
+		contentPanel.add(this.assessementTypeComboBox);
 
 		JLabel descriptionLabel = new JLabel("Descripci\u00F3n");
 		descriptionLabel.setFont(new Font("Arial", Font.BOLD, 11));
-		descriptionLabel.setBounds(10, 98, 162, 14);
+		descriptionLabel.setBounds(10, 68, 65, 14);
 		contentPanel.add(descriptionLabel);
 
-		JTextPane DescriptiontextPane = new JTextPane();
-		DescriptiontextPane.setFont(new Font("Arial", Font.PLAIN, 11));
-		DescriptiontextPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		DescriptiontextPane.setBackground(Color.WHITE);
-		DescriptiontextPane.setBounds(10, 124, 668, 60);
-		contentPanel.add(DescriptiontextPane);
+		JScrollPane descriptionScrollPane = new JScrollPane();
+		descriptionScrollPane.setBounds(10, 85, 701, 88);
+		contentPanel.add(descriptionScrollPane);
+
+		this.descriptionTextArea = new JTextArea();
+		descriptionScrollPane.setViewportView(this.descriptionTextArea);
+		this.descriptionTextArea.setLineWrap(true);
+		this.descriptionTextArea.setWrapStyleWord(true);
+		this.descriptionTextArea.setFont(new Font("Arial", Font.PLAIN, 11));
+		this.descriptionTextArea.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		this.descriptionTextArea.setBackground(Color.WHITE);
 
 		JLabel instrumentLabel = new JLabel("Instrumento");
 		instrumentLabel.setFont(new Font("Arial", Font.BOLD, 11));
-		instrumentLabel.setBounds(10, 203, 162, 14);
+		instrumentLabel.setBounds(10, 185, 69, 14);
 		contentPanel.add(instrumentLabel);
 
-		JTextPane instrumentTextPane = new JTextPane();
-		instrumentTextPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		instrumentTextPane.setBounds(10, 228, 535, 65);
-		contentPanel.add(instrumentTextPane);
+		JScrollPane instrumentScrollPane = new JScrollPane();
+		instrumentScrollPane.setBounds(10, 202, 654, 88);
+		contentPanel.add(instrumentScrollPane);
 
-		JLabel selectionLabel = new JLabel("Tipo de Evaluaci\u00F3n");
-		selectionLabel.setFont(new Font("Arial", Font.BOLD, 11));
-		selectionLabel.setBounds(10, 24, 131, 14);
-		contentPanel.add(selectionLabel);
+		this.instrumentTextArea = new JTextArea();
+		instrumentScrollPane.setViewportView(this.instrumentTextArea);
+		this.instrumentTextArea.setEnabled(false);
+		this.instrumentTextArea.setBorder(new LineBorder(Color.LIGHT_GRAY));
 
-		JComboBox<Subject> assessementTypcomboBox = new JComboBox<Subject>();
-		assessementTypcomboBox.setBounds(10, 49, 367, 29);
-		contentPanel.add(assessementTypcomboBox);
+		this.addInstrumentbutton = new JButton(Resources.ADD_ELEMENT_ICON);
+		this.addInstrumentbutton.setBounds(676, 202, 35, 35);
+		contentPanel.add(this.addInstrumentbutton);
 
-		JButton selectionbutton = new JButton("Seleccionar");
-		selectionbutton.setFont(new Font("Arial", Font.BOLD, 12));
-		selectionbutton.setBounds(578, 242, 100, 30);
-		contentPanel.add(selectionbutton);
+		JButton removeInstrumentButton = new JButton(Resources.DELETE_ELEMENT_ICON);
+		removeInstrumentButton.setBounds(676, 255, 35, 35);
+		contentPanel.add(removeInstrumentButton);
+
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 302, 701, 2);
+		contentPanel.add(separator);
+
+		this.commitButton = new JButton("Aceptar");
+		this.commitButton.setFont(new Font("Arial", Font.BOLD, 12));
+		this.commitButton.setBounds(501, 316, 100, 30);
+		contentPanel.add(this.commitButton);
+
+		this.rejectButton = new JButton("Cancelar");
+		this.rejectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ReactiveFormDialog.this.dispose();
+			}
+		});
+		this.rejectButton.setFont(new Font("Arial", Font.BOLD, 12));
+		this.rejectButton.setBounds(611, 316, 100, 30);
+		contentPanel.add(this.rejectButton);
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		this.descriptionTextArea.setEnabled(enabled);
+
+		this.addInstrumentbutton.setEnabled(enabled);
+		this.commitButton.setEnabled(enabled);
+		this.rejectButton.setEnabled(enabled);
+	}
+
+	/**
+	 * La función de carga del combo box de tipo de evaluación.
+	 */
+	private void loadInstrumentLevelOneComboBox() {
+		this.assessementTypeComboBox.removeAllItems();
+
+		// Volvemos a cargar el combo.
+		for (InstrumentTypeInterface item : InstrumentType.values()) {
+			this.assessementTypeComboBox.addItem(item);
+		}
+		this.assessementTypeComboBox.setSelectedIndex(-1);
+		this.assessementTypeComboBox.setEnabled(true);
+	}
+
+	/**
+	 * La función encargada de cargar el combo de tipos de instrumentos a cargar dentro de este reactivo para poder clasificarlo como formal,
+	 * semiformal o no formal.
+	 * 
+	 * @param instrumentClass
+	 *            La clase de los instrumentos que podemos cargar.
+	 */
+	private void loadInstrumentClassInCombo(Class<? extends Instrument> instrumentClass) {
+		if (instrumentClass != null) {
+			for (InstrumentTypeInterface item : InstrumentType.values()) {
+				if (item.getInstrumentClass().isAssignableFrom(instrumentClass)) {
+					this.assessementTypeComboBox.setSelectedItem(item);
+					this.assessementTypeComboBox.setEnabled(false);
+					break;
+				}
+			}
+		} else {
+			this.assessementTypeComboBox.setEnabled(true);
+		}
 	}
 
 	/**
@@ -111,8 +222,7 @@ public class ReactiveFormDialog extends JDialog {
 	}
 
 	/**
-	 * La función encargada de cargar los datos que tenemos dentro de la ventana dentro del reactivo que estamos
-	 * editando.
+	 * La función encargada de cargar los datos que tenemos dentro de la ventana dentro del reactivo que estamos editando.
 	 * 
 	 * @throws CheckedException
 	 *             En caso de que ocurra un fallo a la hora de cargar el reactivo desde la ventana.
@@ -136,6 +246,25 @@ public class ReactiveFormDialog extends JDialog {
 	}
 
 	/**
+	 * La función encargada de configurar la ventana para dar de alta un nuevo reactivo.
+	 * 
+	 * @param instrumenClass
+	 *            La clase de los instrumentos que vamos a poder asignarle al reactivo.
+	 * @return La ventana con el formulario configurado para dar de alta un reactivo.
+	 */
+	public ReactiveFormDialog createNewDialog(Class<? extends Instrument> instrumentClass) {
+		this.setTitle(HolderMessage.getMessage("reactive.form.dialog.title.new"));
+
+		this.loadInstrumentLevelOneComboBox();
+		this.loadInstrumentClassInCombo(instrumentClass);
+
+		this.reactive = new Reactive();
+		this.emptyFields();
+
+		return this;
+	}
+
+	/**
 	 * La función encargada de configurar la ventana para la edición de un reactivo.
 	 * 
 	 * @param reactive
@@ -143,16 +272,14 @@ public class ReactiveFormDialog extends JDialog {
 	 * @return La ventana del formulario ya configurado para la edición del reactivo.
 	 */
 	public ReactiveFormDialog createEditDialog(Reactive reactive) {
-		// TODO gmazzali hacer lo de la configuración de la ventana de reactivo para un edición.
-	}
+		this.setTitle(HolderMessage.getMessage("reactive.form.dialog.title.edit"));
 
-	/**
-	 * La función encargada de configurar la ventana para dar de alta un nuevo reactivo.
-	 * 
-	 * @return La ventana con el formulario configurado para dar de alta un reactivo.
-	 */
-	public ReactiveFormDialog createNewDialog() {
-		// TODO gmazzali hacer lo de la configuración de la ventana de reactivo para un alta.
+		this.loadInstrumentLevelOneComboBox();
+
+		this.reactive = reactive;
+		this.fromReactiveToDialog();
+
+		return this;
 	}
 
 	/**
@@ -160,8 +287,15 @@ public class ReactiveFormDialog extends JDialog {
 	 */
 	public static void main(String[] args) {
 		try {
-			ReactiveFormDialog dialog = new ReactiveFormDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+			UIManager.setLookAndFeel(new NimbusLookAndFeel());
+			String[] files =
+				{ "/com/proyecto/spring/general-application-context.xml" };
+			HolderApplicationContext.initApplicationContext(files);
+
+			ReactiveFormDialog dialog = HolderApplicationContext.getContext().getBean(ReactiveFormDialog.class)
+					.createNewDialog(ChoiceInstrument.class);
+			dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
