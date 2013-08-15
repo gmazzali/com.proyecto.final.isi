85dbec6fa206ba8a052eb9960c15483c40b6be3b
guillermomazzali@gmail.com <guillermomazzali@gmail.com@4db54f59-3118-ff34-ce64-8256e353c912>
2013-08-15 14:07:40 +0000

    Cambios en el conversor de tipos de reactivo a tipos de instrumento por falla al momento de tomar un listado de tipos de reactivos nulo.

--- trunk/src/main/java/com/proyecto/converter/ReactiveTypeToInstrumentTypeConverter.java
+++ trunk/src/main/java/com/proyecto/converter/ReactiveTypeToInstrumentTypeConverter.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import com.proyecto.model.material.instrument.type.InstrumentType;
 import com.proyecto.model.material.reactive.type.ReactiveType;
+import com.proyecto.model.material.reactive.type.impl.ReactiveTypeImpl;
 
 /**
  * La clase que nos permite convertir un listado de reactivos habilitados en un listado de los instrumentos habilitados.
@@ -14,15 +15,33 @@ import com.proyecto.model.material.reactive.type.ReactiveType;
  */
 public class ReactiveTypeToInstrumentTypeConverter {
 
-	public static InstrumentType[] converter(ReactiveType[] reactiveType) {
+	/**
+	 * La función encargada de tomar un listado de los tipos de reactivos y retornar un listado de los posibles tipos de instrumentos para ese
+	 * conjunto de tipos de reactivos.
+	 * 
+	 * @param reactiveTypes
+	 *            El conjunto de tipos de reactivos que vamos a analizar, puede ser nulo.
+	 * @return El listado de los tipos de instrumentos válidos para elo conjunto de tipos de reactivo recibido, en caso de recibir un conjunto de
+	 *         tipos de reactivos nulo, retornamos un listado de los tipos de instrumento válido para todo los tipos de reactivos.
+	 */
+	public static InstrumentType[] converter(ReactiveType[] reactiveTypes) {
 
 		// El set de los instrumentos.
 		Set<InstrumentType> instrumentType = new HashSet<InstrumentType>();
 
-		// Recorremos el listado de los tipos de reactivos.
-		for (ReactiveType type : reactiveType) {
-			for (InstrumentType instrument : type.getInstrumentsTypeAllowed()) {
-				instrumentType.add(instrument);
+		// Si el listado recibido no es nulo, lo recorremos.
+		if (reactiveTypes != null) {
+			// Recorremos el listado de los tipos de reactivos.
+			for (ReactiveType type : reactiveTypes) {
+				for (InstrumentType instrument : type.getInstrumentsTypeAllowed()) {
+					instrumentType.add(instrument);
+				}
+			}
+		} else {
+			for (ReactiveType type : ReactiveTypeImpl.values()) {
+				for (InstrumentType instrument : type.getInstrumentsTypeAllowed()) {
+					instrumentType.add(instrument);
+				}
 			}
 		}
 
