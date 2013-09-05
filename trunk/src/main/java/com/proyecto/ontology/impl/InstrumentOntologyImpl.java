63aaca0219b32b4579f335782dfb913a98e5e153
guillermomazzali@gmail.com <guillermomazzali@gmail.com@4db54f59-3118-ff34-ce64-8256e353c912>
2013-09-05 18:40:37 +0000

    Carga de la estructura de manejo de la ontología. Posibles errores en el sistema.

---
+++ trunk/src/main/java/com/proyecto/ontology/impl/InstrumentOntologyImpl.java
@@ -0,0 +1,24 @@
+package com.proyecto.ontology.impl;
+
+import com.common.util.annotations.Service;
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.proyecto.model.material.instrument.Instrument;
+import com.proyecto.model.material.reactive.Reactive;
+import com.proyecto.ontology.InstrumentOntology;
+
+/**
+ * La clase que implementa la interfaz que define el comportamiento de la carga de instrumentos dentro de una ontología.
+ * 
+ * @author Guillermo Mazzali
+ * @version 1.0
+ */
+@Service
+public class InstrumentOntologyImpl implements InstrumentOntology {
+
+	@Override
+	public <I extends Instrument> Individual loadInstrumentToOntology(OntModel ontology, Reactive reactive, I instrument) {
+		// TODO gmazzali Hacer el metodo de carga de un instrumento en una ontología.
+		return null;
+	}
+}
---
+++ trunk/src/main/java/com/proyecto/ontology/impl/ReactiveOntologyImpl.java
@@ -0,0 +1,63 @@
+package com.proyecto.ontology.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.common.util.annotations.Service;
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.ontology.DatatypeProperty;
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.proyecto.model.material.activity.Activity;
+import com.proyecto.model.material.reactive.Reactive;
+import com.proyecto.ontology.ConstantsOntology;
+import com.proyecto.ontology.InstrumentOntology;
+import com.proyecto.ontology.ReactiveOntology;
+
+/**
+ * La clase que implementa la interfaz que define el comportamiento de la carga de reactivos dentro de una ontología.
+ * 
+ * @author Guillermo Mazzali
+ * @version 1.0
+ */
+@Service
+public class ReactiveOntologyImpl implements ReactiveOntology {
+	
+	/**
+	 * El servicio de carga de los instrumentos a la ontología.
+	 */
+	@Autowired
+	private InstrumentOntology instrumentOntology;
+
+	@Override
+	public <R extends Reactive> Individual loadReactiveToOntology(OntModel ontology, Activity activity, R reactive) {
+		String className = ConstantsOntology.NAMESPACE + reactive.getClass().getSimpleName();
+		String individualName = className + "_" + reactive.getId();
+
+		// Creamos la clase y la instancia del reactivo.
+		OntClass reactiveClass = ontology.createClass(className);
+		Individual reactiveIndividual = reactiveClass.createIndividual(individualName);
+
+		// Creamos las relaciones.
+		DatatypeProperty haveDescription = ontology.createDatatypeProperty(ConstantsOntology.PROPERTY_REACTIVE_HAVE_DESCRIPTION);
+		DatatypeProperty haveReactive = ontology.createDatatypeProperty(ConstantsOntology.PROPERTY_REACTIVE_HAVE_INSTRUMENT);
+
+		// Creamos los literales.
+		Literal description = ontology.createTypedLiteral(reactive.getDescription(), XSDDatatype.XSDstring);
+
+		// Creamos las carga de los datos.
+		List<Statement> statements = new ArrayList<Statement>();
+		statements.add(ontology.createLiteralStatement(reactiveIndividual, haveDescription, description));
+		statements.add(ontology.createLiteralStatement(reactiveIndividual, haveReactive,
+				this.instrumentOntology.loadInstrumentToOntology(ontology, reactive, reactive.getInstrument())));
+
+		ontology.add(statements);
+
+		return reactiveIndividual;
+	}
+}
